d67a6aed7e943f5f8be22e1475bb8566c4d1e788
tianxiaoliang <tianxiaoliang@pwrd.com>
2016-12-06 19:05:25 +0800

    add kafka

---
+++ kafka-demo/src/main/java/com/xiao/storm/kafka/KafkaProducerUtils.java
@@ -0,0 +1,17 @@
+package com.xiao.storm.kafka;
+
+/**
+ * Created by xiaoliang
+ * 2016.12.06 15:54
+ *
+ * @Version 1.0
+ */
+public class KafkaProducerUtils {
+
+    public static final String CONFIG_FILE = "config/kafka.properties";
+
+    private KafkaProducerUtils() {
+
+    }
+
+}

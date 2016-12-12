c7bb68723c5c4d0f619a95aa513a3006b640420d
tianxiaoliang <tianxiaoliang@pwrd.com>
2016-12-12 17:28:00 +0800

    kafka

---
+++ kafka-demo/src/main/java/com/xiao/storm/bolt/Topology.java
@@ -0,0 +1,15 @@
+package com.xiao.storm.bolt;
+
+/**
+ * Created by xiaoliang
+ * 2016.12.09 16:40
+ *
+ * @Version 1.0
+ */
+public class Topology {
+
+
+    public static void main(String[] args) {
+
+    }
+}
